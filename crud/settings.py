9209fc5061bc448e5990decee8a1501e8387a8da
Ashraful Islam Emon <ashraful.emon26@gmail.com>
2022-06-02 10:39:05 +0600

    collect static

--- crud/settings.py
+++ crud/settings.py
@@ -130,6 +130,7 @@ MEDIA_URL = ''
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
