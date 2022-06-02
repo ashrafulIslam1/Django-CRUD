7e02b96874efafea9d44c9d53636e3b7c11536e3
Ashraful Islam Emon <ashraful.emon26@gmail.com>
2022-06-02 09:46:08 +0600

    adding white noise into middleware and adding static_root and adding allowed_hosts and doing debug = false

--- crud/settings.py
+++ crud/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6dy0ivwg1&xle_vhe0w!)(k)mau69=ux9)fp$lc$ykjrei_fsy'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['django-crud1.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
